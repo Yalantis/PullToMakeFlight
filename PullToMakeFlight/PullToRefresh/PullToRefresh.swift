9ca585630b17c0ee69a18c17a851d4036818a9bd
Artem Sidorenko <artem.sydorenko@yalantis.com>
2015-07-27 16:02:59 +0300

    Update project to Swift 2.0

--- PullToMakeFlight/PullToRefresh/PullToRefresh.swift
+++ PullToMakeFlight/PullToRefresh/PullToRefresh.swift
@@ -99,7 +99,7 @@ public class PullToRefresh: NSObject {
     private let contentOffsetKeyPath = "contentOffset"
     private var previousScrollViewOffset: CGPoint = CGPointZero
     
-    override public func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<()>) {
+    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<()>) {
         if (context == &KVOContext && keyPath == contentOffsetKeyPath && object as? UIScrollView == scrollView) {
             let offset = previousScrollViewOffset.y + scrollViewDefaultInsets.top
             let refreshViewHeight = refreshView.frame.size.height
@@ -146,7 +146,7 @@ public class PullToRefresh: NSObject {
 
 // MARK: - State enumeration
 
-public enum State:Equatable, Printable {
+public enum State:Equatable, CustomStringConvertible {
     case Inital, Loading, Finished
     case Releasing(progress: CGFloat)
     
@@ -165,7 +165,7 @@ public func ==(a: State, b: State) -> Bool {
     case (.Inital, .Inital): return true
     case (.Loading, .Loading): return true
     case (.Finished, .Finished): return true
-    case (.Releasing(let a), .Releasing(let b)): return true
+    case (.Releasing, .Releasing): return true
     default: return false
     }
 }
@@ -180,7 +180,7 @@ class DefaultRefreshView: UIView {
         commonInit()
     }
     
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
     }
